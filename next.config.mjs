b3fbe9ae3c3c27d1c19c15ae3b067023575b885c
Ondanaoto <n.onda.go20@gmail.com>
2024-02-17 12:01:42 +0900

    rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/out',
+    assetPrefix: '/out/',
 };
 
 export default nextConfig;
--- scripts/build.js
+++ scripts/build.js
@@ -1,18 +1,34 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
-// next build を実行
+const deleteFolderRecursive = function(directoryPath) {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach((file) => {
+      const curPath = path.join(directoryPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // ディレクトリの場合は再帰的に削除
+        deleteFolderRecursive(curPath);
+      } else { // ファイルの場合は削除
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(directoryPath);
+  }
+};
+
+// yarn build を実行
 exec('yarn build', (err) => {
   if (err) {
     console.error(`ビルドエラー: ${err}`);
     return;
   }
 
-  // out ディレクトリが存在する場合、docs にリネーム
-  if (fs.existsSync('./out')) {
-    fs.renameSync('./out', './docs');
-    console.log('ビルド完了し、out ディレクトリを docs にリネームしました。');
-  } else {
-    console.error('out ディレクトリが見つかりません。');
+  // docs ディレクトリが既に存在し、空でない場合は削除
+  if (fs.existsSync('./docs')) {
+    deleteFolderRecursive('./docs');
   }
+
+  // out ディレクトリを docs にリネーム
+  fs.renameSync('./out', './docs');
+  console.log('ビルド完了し、out ディレクトリを docs にリネームしました。');
 });
